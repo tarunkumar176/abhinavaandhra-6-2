c7b1e7171c2583b2254da22cbc167e010adceab6
vamsi-naidu7 <vn8644895@gmail.com>
2025-12-06 15:19:56 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
         historyApiFallback: true,
         proxy: {
           '/uploads': {
-            target: 'http://localhost:5000',
+            target: 'https://abhinavandhra-backend.onrender.com',
             changeOrigin: true,
           }
         }
